65f6f3782f3ff5a1bdd7e1802cb93677529e289e
sk <semaria.eyzac@gmail.com>
2018-05-11 10:36:45 -0700

    the foods array from food-list component put in a new constant array

--- src/app/mock-foods.ts
+++ src/app/mock-foods.ts
@@ -1,1 +1,8 @@
 import { Food } from './food.model';
+
+export const FOODS: Food[] = [
+new Food("Tofurkey", "Tofu", "Protein", 450, "Stir fry onion & garlic first, then add the tofu which is cut in small pieces, cook for few min", 1),
+new Food("Lentil-stew", "Lentil", "Protein", 500, "Boil water & onion then add the lentil and boil it for few min, serve it as needed", 2),
+new Food("Fried-spinach-mushroom", "Spinach & mushroom", "Carb", 400, "Garlic fry the spinach & mushroom and serve it with either taco or pasta", 3),
+new Food("Fruit-salad", "Mango & guava", "Vitamin C", 200, "Cut mango & guava or any other fruits & serve it", 4)
+];
