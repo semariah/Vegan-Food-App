be1f2f0d6bca6d453c8945a95f5fbc2917c0f9b5
sk <semaria.eyzac@gmail.com>
2018-05-11 09:27:34 -0700

    add the routing path and import to the food-list component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
+import { FoodListComponent } from './food-list/food-list.component';
 
 
 const appRoutes: Routes = [
@@ -12,7 +13,13 @@ const appRoutes: Routes = [
   {
     path: 'about',
     component: AboutComponent
-  }
+  },
+  {
+    path: 'food-list',
+    component: FoodListComponent
+  },
+
+
 
 ];
 
