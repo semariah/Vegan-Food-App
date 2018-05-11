83b03ae8e83c69119134e450f9bfbafe90ae00b4
sk <semaria.eyzac@gmail.com>
2018-05-11 09:36:04 -0700

    create it and add the export class food

---
+++ src/app/food.model.ts
@@ -0,0 +1,4 @@
+export class Food {
+  public organic: boolean = false;
+  constructor(public name: string, public ingredient: string, public diet: string, public calories: number, public method: string, public id: number) {}
+}
