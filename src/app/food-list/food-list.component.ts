397131ec645e3de2d48f2bd2a11cd143b49da2fa
sk <semaria.eyzac@gmail.com>
2018-05-11 14:46:56 -0700

    add a property containing current route in the export class

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -14,12 +14,14 @@ import { FirebaseListObservable } from 'angularfire2/database';
 })
 export class FoodListComponent implements OnInit {
   foods: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private foodService: FoodService) { }
 
 
   ngOnInit(){
     this.foods = this.foodService.getFoods();
+
   }
 
   goToDetailPage(clickedFood: Food) {
