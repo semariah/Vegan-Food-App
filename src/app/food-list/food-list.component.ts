c9d68a838871995ecf57a000ec4094f1cc6184cc
sk <semaria.eyzac@gmail.com>
2018-05-11 09:37:48 -0700

    import the food class and add the food lists hard code

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Food } from '../food.model';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnInit {
+export class FoodListComponent {
+  foods: Food[] = [
+  new Food("Tofurkey", "Tofu", "Protein", 450, "Stir fry onion & garlic first, then add the tofu which is cut in small pieces, cook for few min", 1),
+  new Food("Lentil-stew", "Lentil", "Protein", 500, "Boil water & onion then add the lentil and boil it for few min, serve it as needed", 2),
+  new Food("Fried-spinach-mushroom", "Spinach & mushroom", "Carb", 400, "Garlic fry the spinach & mushroom and serve it with either taco or pasta", 3),
+  new Food("Fruit-salad", "Mango & guava", "Vitamin C", 200, "Cut mango & guava or any other fruits & serve it", 4)
+];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+
 
 }
