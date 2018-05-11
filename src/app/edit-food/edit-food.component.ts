5f90c70b9018faa3f56867294890ab78ff954cff
sk <semaria.eyzac@gmail.com>
2018-05-11 15:04:36 -0700

    import Input from angular core and define @Input named selectedFood

--- src/app/edit-food/edit-food.component.ts
+++ src/app/edit-food/edit-food.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Food } from '../food.model';
+
 
 @Component({
   selector: 'app-edit-food',
   templateUrl: './edit-food.component.html',
   styleUrls: ['./edit-food.component.css']
 })
+
 export class EditFoodComponent implements OnInit {
+  @Input() selectedFood;
 
   constructor() { }
 
