c874a55396dc80d3179162595a288483a9ca7e67
sk <semaria.eyzac@gmail.com>
2018-05-11 10:57:35 -0700

    define a method that returns a specific food

--- src/app/food.service.ts
+++ src/app/food.service.ts
@@ -11,4 +11,13 @@ export class FoodService {
     return FOODS;
   }
 
+  getFoodById(foodId: number){
+    for (var i = 0; i <= FOODS.length - 1; i++) {
+      if (FOODS[i].id === foodId) {
+        return FOODS[i];
+      }
+    }
+  }
+
+
 }
