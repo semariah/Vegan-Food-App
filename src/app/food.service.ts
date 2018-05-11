bb87bf42ff6f16494c8c522b32c320f11e880a01
sk <semaria.eyzac@gmail.com>
2018-05-11 10:40:29 -0700

    creat a new food service file for injection purpose

---
+++ src/app/food.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Food } from './food.model';
+import { FOODS } from './mock-foods';
+
+@Injectable()
+export class FoodService {
+
+  constructor() { }
+
+}
