b40e5242622f8623abc66198ebb895bfbc78fdd1
sk <semaria.eyzac@gmail.com>
2018-05-11 10:45:31 -0700

    import Food service, update constructor with foodservice object and register provider

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../food.model';
 import { Router } from '@angular/router';
+import { FoodService } from '../food.service';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  styleUrls: ['./food-list.component.css'],
+  providers: [FoodService]
 })
 export class FoodListComponent {
   foods: Food[];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private foodService: FoodService) { }
 
   goToDetailPage(clickedFood: Food) {
      this.router.navigate(['foods', clickedFood.id]);
