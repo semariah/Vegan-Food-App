da6524db52cd47fcc9551c4a6319fa1cea549345
sk <semaria.eyzac@gmail.com>
2018-05-11 10:35:39 -0700

    move the foods array to a new file called mock-foods.ts

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -8,12 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent {
-  foods: Food[] = [
-  new Food("Tofurkey", "Tofu", "Protein", 450, "Stir fry onion & garlic first, then add the tofu which is cut in small pieces, cook for few min", 1),
-  new Food("Lentil-stew", "Lentil", "Protein", 500, "Boil water & onion then add the lentil and boil it for few min, serve it as needed", 2),
-  new Food("Fried-spinach-mushroom", "Spinach & mushroom", "Carb", 400, "Garlic fry the spinach & mushroom and serve it with either taco or pasta", 3),
-  new Food("Fruit-salad", "Mango & guava", "Vitamin C", 200, "Cut mango & guava or any other fruits & serve it", 4)
-];
+  foods: Food[];
 
   constructor(private router: Router) { }
 
