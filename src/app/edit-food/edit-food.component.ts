46380e5c655a0e453028c9506704fd547eaae7a3
sk <semaria.eyzac@gmail.com>
2018-05-11 16:11:51 -0700

    update after error and use of new input model format for the ngModel

--- src/app/edit-food/edit-food.component.ts
+++ src/app/edit-food/edit-food.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Food } from '../food.model';
+import { FoodService } from '../food.service';
 
 
 @Component({
   selector: 'app-edit-food',
   templateUrl: './edit-food.component.html',
-  styleUrls: ['./edit-food.component.css']
+  styleUrls: ['./edit-food.component.css'],
+  providers: [FoodService]
 })
 
 export class EditFoodComponent implements OnInit {
   @Input() selectedFood;
 
-  constructor() { }
+  constructor(private foodService: FoodService) { }
 
   ngOnInit() {
   }
