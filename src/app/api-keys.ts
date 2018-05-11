e3845b6d8d75fd3826d0b43f909d449cdf4c2eb2
sk <semaria.eyzac@gmail.com>
2018-05-11 11:18:52 -0700

    add an api key from firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyDfZqVkrWuBlJ5vccKu0_znaPpjE9_o_5o",
+  authDomain: "veggi-1abda.firebaseapp.com",
+  databaseURL: "https://veggi-1abda.firebaseio.com",
+  projectId: "veggi-1abda",
+  storageBucket: "",
+  messagingSenderId: "489636102497"
+
+}
