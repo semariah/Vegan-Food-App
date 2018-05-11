9234d16b4fc840ff07845b8d2daeac3cb0bb74a2
sk <semaria.eyzac@gmail.com>
2018-05-11 10:54:56 -0700

    import the food service in to food detail component, add provider array with food service and instantiate service in component constructor

--- src/app/food-detail/food-detail.component.ts
+++ src/app/food-detail/food-detail.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Food } from '../food.model';
+import { FoodService } from '../food.service';
 
 @Component({
   selector: 'app-food-detail',
   templateUrl: './food-detail.component.html',
-  styleUrls: ['./food-detail.component.css']
+  styleUrls: ['./food-detail.component.css'],
+  providers: [FoodService]
 })
 export class FoodDetailComponent implements OnInit {
   foodId: number = null;
 
-  constructor(private route: ActivatedRoute, private location: Location) { }
+  constructor(private route: ActivatedRoute, private location: Location, private foodService: FoodService) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
